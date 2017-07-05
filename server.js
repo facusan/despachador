fc978310b26c977cbd8e2df042b39b5dd7616a7b
facusan <facundosg@hotmail.com>
2017-07-05 13:32:52 -0300

    var port = process.env.PORT || 8080;

--- server.js
+++ server.js
@@ -163,7 +163,8 @@ function mycomparator(a,b) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
     // listen (start app with node server.js) ======================================
-    app.listen(80);
-    console.log("Despachador on port 80");
+    var port = process.env.PORT || 8080;
+    app.listen(port);
+    console.log("Despachador on port " + port);
     ////
     
