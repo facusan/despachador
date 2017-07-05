716e392d7a392e37c3d64c2c790e6d433adb4343
facusan <facundosg@hotmail.com>
2017-07-05 12:07:13 -0300

    puerto 80

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ function mycomparator(a,b) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
     // listen (start app with node server.js) ======================================
-    app.listen(8080);
-    console.log("Despachador on port 8080");
+    app.listen(80);
+    console.log("Despachador on port 80");
     ////
     
